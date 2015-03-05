5a2fb517e55b8bd98d57f601f3dfdd60067bb8c0
Sam Yaple <sam@yaple.net>
2015-03-04 23:13:04 -0600

    Changed OSD creation
    OSDs are now partitioned and formated with ceph-disk to create a more
    traditional OSD. Additionally, the journal is no accessible with raw
    access for performance.
    
    The filesystem is still mounted outside of the container and then
    bind mounted in at the appropriate OSD location.

--- ansible/roles/docker_build/ceph/files/ubuntu/ceph/osd.sh
+++ ansible/roles/docker_build/ceph/files/ubuntu/ceph/osd.sh
@@ -5,24 +5,37 @@ set -o errexit
 main() {
     initial_setup
 
-
     if [[ -n "${INITIALIZE_OSD}" ]]; then
     	if [[ ! -n "${HOSTNAME}" ]]; then
                 variable_name="HOSTNAME"
             	unset_variable
 	fi
 
+	if [[ ! -n "${OSD_DEV}" ]]; then
+                variable_name="OSD_DEV"
+            	unset_variable
+	fi
+
+	parted "${OSD_DEV}" -s -- mklabel gpt
+	ceph-disk prepare "${OSD_DEV}"
+
+
 	osd_id="$(ceph osd create)"
-	osd_dir="${osd_basedir}/${CEPH_CLUSTER_NAME}-${osd_id}"
-	ln -s "new_osd" "${osd_dir}"
+        osd_dir="/var/lib/ceph/osd/${CEPH_CLUSTER_NAME}-${osd_id}"
+
+	mkdir -p "${osd_dir}"
+
+	mount "${OSD_DEV}1" "${osd_dir}"
 
 	ceph-osd -i "${osd_id}" --mkfs --mkkey
 	ceph auth add "osd.${osd_id}" osd 'allow *' mon 'allow profile osd' \
-							-i "${osd_dir}/keyring"
+					     	        -i "${osd_dir}/keyring"
 
 	ceph osd crush add-bucket "${HOSTNAME}" host
 	ceph osd crush move "${HOSTNAME}" root=default
 	ceph osd crush add "${osd_id}" 1.0 host="${HOSTNAME}"
+
+	umount "${osd_dir}"
 
 	exit 0
     fi
@@ -32,7 +45,7 @@ main() {
             unset_variable
     fi
 
-    osd_dir="${osd_basedir}/${CEPH_CLUSTER_NAME}-${OSD_ID}"
+    osd_dir="/var/lib/ceph/osd/${CEPH_CLUSTER_NAME}-${OSD_ID}"
 }
 
 initial_setup() {
@@ -43,7 +56,6 @@ initial_setup() {
 
     ceph_conf="/etc/ceph/${CEPH_CLUSTER_NAME}.conf"
     keyring_admin="/etc/ceph/${CEPH_CLUSTER_NAME}.client.admin.keyring"
-    osd_basedir="/var/lib/ceph/osd/"
 
     if [[ ! -e "${ceph_conf}" ]]; then
         file="${ceph_conf}"
