5a254a33c501a1273c08dc16bceba779c1f61334
Sam Yaple <sam@yaple.net>
2015-03-15 04:48:55 -0500

    added keepalived container

---
+++ ansible/roles/docker_build/files/ubuntu/keepalived/keepalived.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -o errexit
+
+main() {
+    initial_setup
+}
+
+initial_setup() {
+    keepalived_conf="/etc/keepalived/keepalived.conf"
+
+    if [[ ! -e "${keepalived_conf}" ]]; then
+        file="${keepalived_conf}"
+        missing_file
+    fi
+}
+
+unset_variable() {
+    echo "ERROR: \"${variable_name}\" is a required variable"
+    exit 31
+}
+
+missing_file() {
+    echo "ERROR: \"${file}\" does not exist"
+    exit 32
+}
+
+main
+
+exec /usr/sbin/keepalived
