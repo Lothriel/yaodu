5cffac4f1f9930eadb306757d689f907b8db775a
Sam Yaple <sam@yaple.net>
2015-03-16 23:49:02 -0500

    abstracted error code handling

--- ansible/roles/docker_build/files/ubuntu/haproxy/entrypoint.sh
+++ ansible/roles/docker_build/files/ubuntu/haproxy/entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -o errexit
 
+source /opt/yaodu/errors.sh
+
 main() {
     initial_setup
 }
@@ -13,34 +15,6 @@ initial_setup() {
         file="${haproxy_cfg}"
         missing_file
     fi
-}
-
-unset_variable() {
-    echo "ERROR: Variable \"${variable_name}\" is not set"
-    exit 31
-}
-
-missing_file() {
-    echo "ERROR: File \"${file}\" does not exist"
-    exit 32
-}
-
-missing_directory() {
-    echo "ERROR: Directory \"${dir}\" does not exist"
-    exit 33
-}
-
-execution_should_never_reach_here() {
-    cat << EOF
-|-----------------------------------------------------------------------------|
-| ERROR     ERROR     ERROR     ERROR     ERROR     ERROR     ERROR     ERROR |
-|-----------------------------------------------------------------------------|
-| Congratulations! You have reached a point in execution you should not have. |
-| You either broke something, or something broke. I blame you. Either way,    |
-| please report this with any relevant configs and options.                   |
-|-----------------------------------------------------------------------------|
-EOF
-    exit 42
 }
 
 main
