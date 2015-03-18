f491837ae5d35d2aaa05e133add655ca48aed613
Sam Yaple <sam@yaple.net>
2015-03-18 06:17:57 -0500

    Added keystone bootstrap and haproxy listening

---
+++ ansible/roles/docker_build/files/ubuntu/keystone/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+
+source /opt/yaodu/errors.sh
+
+main() {
+    initial_setup
+}
+
+initial_setup() {
+    keystone_dir="/etc/keystone/"
+
+    if [[ ! -e "${keystone_dir}" ]]; then
+        dir="${keystone_dir}"
+        missing_directory
+    fi
+}
+
+main
+
+exec /usr/bin/keystone-all
+exit $?
+
+execution_should_never_reach_here
