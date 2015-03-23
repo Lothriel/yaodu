84b0646e79af474f4888f48a9f9a90ebdc7e4630
Sam Yaple <sam@yaple.net>
2015-03-23 07:39:29 -0500

    Added cinder container and role

---
+++ ansible/roles/docker_build/files/ubuntu/cinder/entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -o errexit
+
+source /opt/yaodu/errors.sh
+
+main() {
+    initial_setup
+}
+
+initial_setup() {
+    cinder_dir="/etc/nova/"
+    services=("api" "scheduler" "volume" "backup")
+
+    if [[ ! -n "${SERVICE}" ]]; then
+        variable_name="SERVICE"
+        unset_variable
+    fi
+
+    if [[ ! "${services[@]}" =~ (^| )"${SERVICE}"($| ) ]]; then
+        invalid_service
+    fi
+
+    if [[ ! -e "${cinder_dir}" ]]; then
+        dir="${cinder_dir}"
+        missing_directory
+    fi
+}
+
+main
+
+exec /usr/bin/env "glance-${SERVICE}"
+
+execution_should_never_reach_here
