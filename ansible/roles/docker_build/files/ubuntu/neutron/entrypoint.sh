bbc6c955d2cf1ecee80fb7f65523255d66dcdd32
Sam Yaple <sam@yaple.net>
2015-03-25 14:19:01 -0500

    Finish out support for openvswitch
    I had to add a patch to ovs_lib.py to support remote ovs management.
    I will be submitting this patch upstream, though it most likely won't
    land until Liberty.

--- ansible/roles/docker_build/files/ubuntu/neutron/entrypoint.sh
+++ ansible/roles/docker_build/files/ubuntu/neutron/entrypoint.sh
@@ -6,11 +6,43 @@ source /opt/yaodu/errors.sh
 
 main() {
     initial_setup
+
+    if [[ "${SERVICE}" == "db" ]]; then
+        openvswitch_dir="/etc/openvswitch/"
+
+        if [[ ! -e "${openvswitch_dir}" ]]; then
+            dir="${openvswitch_dir}"
+            missing_directory
+        fi
+
+        if [[ ! -e "${openvswitch_dir}/conf.db" ]]; then
+            ovsdb-tool create
+        fi
+
+        exec /usr/bin/env ovsdb-server /etc/openvswitch/conf.db -vfile:dbg --remote=ptcp:6633 --log-file=/var/log/openvswitch/ovsdb-server.log
+        execution_should_never_reach_here
+    fi
+
+    if [[ "${SERVICE}" == "switch" ]]; then
+        if [[ ! -n "${REMOTE_IP}" ]]; then
+            variable_name="REMOTE_IP"
+            unset_variable
+        fi
+
+        exec /usr/bin/env ovs-vswitchd "tcp:${REMOTE_IP}:6633" -vfile:dbg --mlockall --log-file=/var/log/openvswitch/ovs-vswitchd.log
+        execution_should_never_reach_here
+    fi
+
+    neutron_dir="/etc/neutron/"
+
+    if [[ ! -e "${neutron_dir}" ]]; then
+        dir="${neutron_dir}"
+        missing_directory
+    fi
 }
 
 initial_setup() {
-    neutron_dir="/etc/neutron/"
-    services=("server" "l3-agent" "dhcp-agent" "plugin-openvswitch-agent")
+    services=("server" "l3-agent" "dhcp-agent" "openvswitch-agent" "db" "switch")
 
     if [[ ! -n "${SERVICE}" ]]; then
         variable_name="SERVICE"
@@ -19,11 +51,6 @@ initial_setup() {
 
     if [[ ! "${services[@]}" =~ (^| )"${SERVICE}"($| ) ]]; then
         invalid_service
-    fi
-
-    if [[ ! -e "${neutron_dir}" ]]; then
-        dir="${neutron_dir}"
-        missing_directory
     fi
 }
 
